9125e2b3f316db333fb3cbfaa9c9c93d75bdf9e2
steven mcwilliams <skmcwilliams@protonmail.com>
2025-11-02 20:26:05 -0600

    BS update, done

--- simplifi/src/simplifi.py
+++ simplifi/src/simplifi.py
@@ -25,94 +25,41 @@ class Simplifi:
         hist['date'] = list(map(str,hist['date']))
         hist['Day'] = hist['date'].apply(lambda x: x.split()[0])
         hist['avg_price'] = (hist['high']+hist['close']+hist['low'])/3
-        hist['returns'] = np.log(hist['close']) - np.log(hist['close'].shift(1))
+        hist['returns'] = np.log(hist['close'])/np.log(hist['close'].shift(1))
         if make_ohlc:
             self.__make_ohlc(hist)
         return hist
 
-    def get_10_year(self):
+    def get_10_year(self)->float:
         """get 10-year treasury from Yahoo Finance"""
         ten_yr = self.yf.history().reset_index()['close'].iloc[-1]
         treas = round(ten_yr/100,4)
         return treas
-    
 
-    def dte_price(self)->pd.DataFrame:
-        prices = np.arange(0.95*self.K,self.K*1.05,0.10)
-        if self.T<=10:
-            days = range(1,self.T+1)
-        else:
-            days = range(1,self.T+1,7)
-        days = [day-1 for day in days]
-        paired = [(price,day) for price in prices for day in days]
-        prices_results = list(map(lambda p: (p[0],self.K,self.r,p[1],self.sd,self.option_type),paired))
-        plot_df = pd.DataFrame(paired,columns=['price','dte'])
-        plot_df['option_value'] = prices_results
-        return plot_df
-
-    def blackscholes(self,
-                    S:float | int, 
-                    K:float | int, 
-                    T:int, 
-                    option_type:str,
-                    plot_data:bool=True)->float:
+    def blackscholes(self)->float:
         """
-        S = Stock Price
-
-        K = Strike Price at Expiration 
-
-        T = Time to Expiration
-
-        option_type = call or put
+        Black-Scholes option pricing model based on current options chain from Yahoo Finance
         """
-        self.K = K
-        self.S = S
-        self.option_type = option_type.lower()
-        allowed_option_types = {"call", "put"}
-        if self.option_type not in allowed_option_types:
-            raise ValueError(f"Invalid option type: '{option_type}'. Option Ttpe must be one of {allowed_option_types}.")
-        else:
-            df = self.get_historical_data() # get daily ticker data from last 30 days
-
-            px.histogram(df['close'],title=f'{self.ticker.upper()} Close Price Last 30 Days').show()
-            px.histogram(df['returns'],title=f'{self.ticker.upper()} Log Returns Last 30 Days').show() #histogram of log returns
-            
-            self.T = T/365
-            # get option data for analysis
-            self.sd = round(df['returns'].std() * sqrt(252),2) # annualize stdev of log daily returns based on # of annual trading days
-            self.r = self.get_10_year()
-            self.price = round(df['close'].iloc[-1],2)
-            
- 
-            print([i for i in np.arange(int(self.price*.75),int(self.price*1.25),0.50)])
-            d1 = (np.log(S/K) + (self.r + self.sd**2/2)* T)/(self.sd*np.sqrt(T))
-            d2 = d1 - self.sd * np.sqrt(T)
-
-            if option_type == "call":
-                value = S * norm.cdf(d1, 0, 1) - K * np.exp(-self.r * T) * norm.cdf(d2, 0, 1)
-            elif option_type == "put":
-                value = K * np.exp(-self.r * T) * norm.cdf(-d2, 0, 1) - S * norm.cdf(-d1, 0, 1)
-            
-            if plot_data:
-                params = {'Price: ':self.price,'Annual Volatility: ':self.sd,'10-yr: ':f'{round(self.r*100,2)}%',"Black-Scholes Value: ":value}
-                for i in params.items():
-                    print(f"{i[0]} {i[1]}")
-                    
-                prices_df = self.dte_price()
-                # print(prices_df)
-
-                px.line(prices_df,x='price',y='option_value',color='dte',title=f'{self.ticker.upper()} {K} dte {option_type}')
-
-
-                go.Figure(data=go.Heatmap(
-                                z=prices_df['option_value'],
-                                x=prices_df['dte'],
-                                y=prices_df['price'],
-                                hoverongaps = True),
-                                )
-            return round(value,2)
         
-    def __make_ohlc(self,df):
+        df = self.get_historical_data() # get daily ticker data from last 30 days
+        px.histogram(df['close'],title=f'{self.ticker.upper()} Close Price Last 30 Days').show()
+        px.histogram(df['returns'],title=f'{self.ticker.upper()} Log Returns Last 30 Days').show() #histogram of log returns
+        options = simplifi.yf.option_chain.reset_index()
+        options['dte'] = (pd.to_datetime(options['expiration']) - pd.to_datetime("now")).dt.days
+        options['t'] = options['dte']/365
+        # get option data for analysis
+        self.sd = round(df['returns'].std() * sqrt(252),2) # annualize stdev of log daily returns based on # of annual trading days
+        self.r = self.get_10_year()
+        options['d1'] = (np.log(options['lastPrice']/options['strike']) + (self.r + self.sd**2/2)* options['t'])/(self.sd*np.sqrt(options['t']))
+        options['d2'] = options['d1'] - self.sd * np.sqrt(options['t'])
+        options['black_scholes_value'] = np.where(options['optionType']=="calls",options['lastPrice']* norm.cdf(options['d1'], 0, 1) - \
+                                                    options['strike'] * np.exp(-self.r * options['t']) * norm.cdf(options['d2'], 0, 1),np.where(
+                                                        options['optionType']=='puts',options['strike'] * np.exp(-self.r * options['t']) * \
+                                                        norm.cdf(-options['d2'], 0, 1) - options['lastPrice'] * norm.cdf(-options['d1'], 0, 1),np.nan))
+
+        return options
+        
+    def __make_ohlc(self,df)->go.Figure:
         ohlc_fig = make_subplots(specs=[[{"secondary_y": True}]]) # creates ability to plot vol and $ change within main plot
     
         #include OHLC (already comes with rangeselector)
@@ -162,101 +109,90 @@ class Simplifi:
             )
         )
         return ohlc_fig.show()
-   
-    class FinViz:
-        def __init__(self):
-            self.ticker = Simplifi.ticker
             
         
-        def fundamentals(self):
-            try:
-                url = f'https://www.finviz.com/quote.ashx?t={self.ticker.lower()}'
-                request = requests.get(url,headers={'User-Agent': 'Mozilla/5.0'})
-                soup = bs(request.text, "lxml")
-                stats = soup.find('table',class_='snapshot-table2')
-                fundamentals =pd.read_html(str(stats))[0]
-                
-                # Clean up fundamentals dataframe
-                fundamentals.columns = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
-                colOne = []
-                colLength = len(fundamentals)
-                for k in np.arange(0, colLength, 2):
-                    colOne.append(fundamentals[f'{k}'])
-                attrs = pd.concat(colOne, ignore_index=True)
-            
-                colTwo = []
-                colLength = len(fundamentals)
-                for k in np.arange(1, colLength, 2):
-                    colTwo.append(fundamentals[f'{k}'])
-                vals = pd.concat(colTwo, ignore_index=True)
-                
-                fundamentals = pd.DataFrame()
-                fundamentals['Attributes'] = attrs
-                fundamentals[f'{self.ticker.upper()}'] = vals
-                fundamentals = fundamentals.set_index('Attributes')
-                fundamentals = fundamentals.T
-                
-                # catch known duplicate column name EPS next Y
-            # fundamentals.rename(columns={fundamentals.columns[28]:'EPS growth next Y'},inplace=True)
-                return fundamentals
-        
-            except Exception as e:
-                return e
-        
-        def ratings(self):
+    def finviz_fundamentals(self)->pd.DataFrame:
+        try:
             url = f'https://www.finviz.com/quote.ashx?t={self.ticker.lower()}'
             request = requests.get(url,headers={'User-Agent': 'Mozilla/5.0'})
             soup = bs(request.text, "lxml")
-            stats = soup.find('table',class_='fullview-ratings-outer')
-            ratings =pd.read_html(str(stats))[0]
-            ratings['date'] = ratings[0].apply(lambda x: x.split()[0][:9])
-            ratings['rating'] = ratings[0].apply(lambda x: x.split()[0][9:])
-            ratings['firm'] = ratings[0].apply(lambda x: x.split()[1])
-            ratings.drop(columns=0,inplace=True)
-            return ratings
+            stats = soup.find('table',class_='snapshot-table2')
+            fundamentals =pd.read_html(str(stats))[0]
+            
+            # Clean up fundamentals dataframe
+            fundamentals.columns = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
+            colOne = []
+            colLength = len(fundamentals)
+            for k in np.arange(0, colLength, 2):
+                colOne.append(fundamentals[f'{k}'])
+            attrs = pd.concat(colOne, ignore_index=True)
         
-
-    class DDM:
-        def __init__(self):
-            self.ticker = Simplifi.ticker
-            self.yf = Simplifi.yf
-            pass
+            colTwo = []
+            colLength = len(fundamentals)
+            for k in np.arange(1, colLength, 2):
+                colTwo.append(fundamentals[f'{k}'])
+            vals = pd.concat(colTwo, ignore_index=True)
+            
+            fundamentals = pd.DataFrame()
+            fundamentals['Attributes'] = attrs
+            fundamentals[f'{self.ticker.upper()}'] = vals
+            fundamentals = fundamentals.set_index('Attributes')
+            fundamentals = fundamentals.T
+            
+            # catch known duplicate column name EPS next Y
+        # fundamentals.rename(columns={fundamentals.columns[28]:'EPS growth next Y'},inplace=True)
+            return fundamentals
+    
+        except Exception as e:
+            return e
         
-        def get_ddm_val(self):
-            last_yr_rate = self.yf.asset_profile("trailingAnnualDividendRate")
-            last_div_rate = self.yf.asset_profile("dividendRate")
-            try:
-                growth = (last_div_rate-last_yr_rate)/last_yr_rate
-            except ValueError as e:
-                print("Dividend growth is incalculable due to lack of historical dividend payment")
-            nxt_yr_div = self.yf.asset_profile("dividendRate") * (1+growth)
-            ddm_val = nxt_yr_div/(self.get_cost_of_equity()-growth)
-            return ddm_val
-
+        
+    def finviz_ratings(self)->pd.DataFrame:
+        url = f'https://www.finviz.com/quote.ashx?t={self.ticker.lower()}'
+        request = requests.get(url,headers={'User-Agent': 'Mozilla/5.0'})
+        soup = bs(request.text, "lxml")
+        stats = soup.find('table',class_='fullview-ratings-outer')
+        ratings =pd.read_html(str(stats))[0]
+        ratings['date'] = ratings[0].apply(lambda x: x.split()[0][:9])
+        ratings['rating'] = ratings[0].apply(lambda x: x.split()[0][9:])
+        ratings['firm'] = ratings[0].apply(lambda x: x.split()[1])
+        ratings.drop(columns=0,inplace=True)
+        return ratings
+        
+        
+    def ddm_valuation(self)->float:
+        last_yr_rate = self.asset_profile("trailingAnnualDividendRate")
+        last_div_rate = self.asset_profile("dividendRate")
+        try:
+            growth = (last_div_rate-last_yr_rate)/last_yr_rate
+        except ValueError as e:
+            print("Dividend growth is incalculable due to lack of historical dividend payment")
+        nxt_yr_div = self.yf.asset_profile("dividendRate") * (1+growth)
+        ddm_val = nxt_yr_div/(self.get_cost_of_equity()-growth)
+        return ddm_val
+ 
                 
-                
-        def get_cost_of_equity(self):
-            beta = self.yf.asset_profile('beta')
-            if type(beta) is str: # string-based tickers are typically new and associated risk is high
-                beta=1.75 # 
-                
-            rm= 0.085
-            rfr = Simplifi.get_10_year()
-            re= rfr+beta*(rm-rfr)
-            print("Valuation based on the following:")
-            print(f"Risk Free Rate: {round(rfr*100,2)}%")
-            print(f"Expected Market Return: {round(rm*100,2)}%")
-            return re
+    def __get_cost_of_equity(self)->float:
+        beta = self.asset_profile('beta')
+        if type(beta) is str: # string-based tickers are typically new and associated risk is high
+            beta=1.75 
+        rm= 0.085 # static expected market return of 8.5%
+        rfr = Simplifi.get_10_year()
+        re= rfr+beta*(rm-rfr)
+        print("Valuation based on the following:")
+        print(f"Risk Free Rate: {round(rfr*100,2)}%")
+        print(f"Expected Market Return: {round(rm*100,2)}%")
+        return re
             
 
             
-
+#%% Test Cases
 if __name__ == '__main__':
     simplifi = Simplifi('AAPL')
-    simplifi.blackscholes(200.00,190.00,7,'call',True)
+    simplifi.blackscholes()
     simplifi.get_historical_data()
-    simplifi.FinViz.ratings()
-    simplifi.FinViz.fundamentals()
+    simplifi.finviz_ratings()
+    simplifi.finviz_fundamentals()
 
 
 
